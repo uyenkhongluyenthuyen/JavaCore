182f8bea095116a193adf668de35ca4c8ddc85e2
小猫咪 <114505681+uyenkhongluyenthuyen@users.noreply.github.com>
2024-06-26 00:14:40 +0700

    final 39

---
+++ src/Lesson39.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Lesson39 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập kích thước hình chữ nhật");
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        if (m > 0 && n > 0) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(" * ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Kích thước phải nguyên dương");
+        }
+    }
+}
---
+++ src/Lesson41.java
@@ -0,0 +1,5 @@
+public class Lesson41 {
+    public static void main(String[] args) {
+        
+    }
+}
